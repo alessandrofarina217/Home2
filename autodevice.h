902e888beb5259adc7e8512dd8a9848f62271930
Alessandro Farina <185652223+alessandrofarina217@users.noreply.github.com>
2025-01-11 09:19:07 +0100

    Update autodevice.h

--- autodevice.h
+++ autodevice.h
@@ -1,11 +1,17 @@
-#ifndef AUTODEVICE_H
-#define AUTODEVICE_H
+#ifndef AUTO_DEVICE_H
+#define AUTO_DEVICE_H
 
 #include "device.h"
 
-class AutoDevice : public Device 
-{
+class AutoDevice : public Device {
+public:
+    void powerOn() override {
+        // Implementation for AutoDevice
+    }
 
-  
-
+    void powerOff() override {
+        // Implementation for AutoDevice
+    }
 };
+
+#endif // AUTO_DEVICE_H
